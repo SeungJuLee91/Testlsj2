cb52b3138bef6ff5d4efea68ac4abba23862e780
SeungJuLee91 <168509333+SeungJuLee91@users.noreply.github.com>
2025-07-22 16:16:57 +0900

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-northeast-1" # 도쿄 리전으로 설정
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-09d8ed8255877048d"
+  instance_type = "t1.micro"
+
+  metadata_options {
+    http_endpoint = "enabled"  # 메타데이터 서비스 사용
+    http_tokens   = "required" # IMDSv2만 허용
+  }
+
+  tags = {
+    Name = "TerraformTest"
+  }
+}
